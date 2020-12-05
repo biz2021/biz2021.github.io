aadeae20b84f212abe221c5215b8a36065b90427
biz2021 <74369270+biz2021@users.noreply.github.com>
2020-12-05 18:27:06 +0100

    update

---
+++ contact.php
@@ -0,0 +1,20 @@
+	<div id="contact-wrapper" wfd-id="103">
+		<div class="container" wfd-id="104">
+			<h3>CONTACT INFORMATION</h3>
+			<br>
+			<span class="head" wfd-id="107"></span>
+
+			<p>We are committed to providing each of our valued clients excellent customer service. <br>If you have any questions or concerns, please do not hesitate to contact one of our professional customer service representatives.</p>
+
+			<br><br>
+			<span class="head" wfd-id="106">Availability</span>
+			<p>Monday-Friday, 9am-9pm EST <br>Saturday-Sunday, 9am-6pm EST
+				<br><br></p>
+			<span class="head" wfd-id="105"></span>
+
+			<address>
+				<strong>CONTACT INFO</strong>
+				<br>Keto BHB<br>Contact Support: <a href="mailto:support@ketobhbreal.com">support@ketobhbreal.com</a>
+			</address>
+		</div>
+	</div>
---
+++ go.php
@@ -0,0 +1,7 @@
+<?php
+
+$lien = "https://hurricanetrk.com/?a=9188&c=109106&s1=339";
+
+
+header("Location: $lien");
+?>
